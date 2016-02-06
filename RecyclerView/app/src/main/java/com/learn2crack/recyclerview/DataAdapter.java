44d7e8d603f14819e5be267df51a225f1354ae67
Raj Amal W <raj.amalw@gmail.com>
2016-02-06 18:09:28 +0530

    Added project files

---
+++ RecyclerView/app/src/main/java/com/learn2crack/recyclerview/DataAdapter.java
@@ -0,0 +1,45 @@
+package com.learn2crack.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
+    private ArrayList<String> countries;
+    private TextView tv_country;
+
+
+    public DataAdapter(ArrayList<String> countries) {
+        this.countries = countries;
+    }
+
+    @Override
+    public DataAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_row, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DataAdapter.ViewHolder viewHolder, int i) {
+
+        tv_country.setText(countries.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return countries.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(View view) {
+            super(view);
+
+            tv_country = (TextView)view.findViewById(R.id.tv_country);
+        }
+    }
+
+}
---
+++ RecyclerView/app/src/main/java/com/learn2crack/recyclerview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.learn2crack.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> countries;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews(){
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        countries = new ArrayList<>();
+        countries.add("Australia");
+        countries.add("India");
+        countries.add("United States of America");
+        countries.add("Germany");
+        countries.add("Russia");
+        RecyclerView.Adapter adapter = new DataAdapter(countries);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                View child = rv.findChildViewUnder(e.getX(), e.getY());
+                int position = rv.getChildAdapterPosition(child);
+                Toast.makeText(getApplicationContext(),countries.get(position),Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+    }
+}
